573ca7e4facc40cd6121d4cbab02434eeb38fa39
JerryLeeCS <jerryleeatwork@gmail.com>
2021-03-13 11:28:38 -0800

    Added Ropsten config and contracts.

--- truffle-config.js
+++ truffle-config.js
@@ -1,4 +1,7 @@
 const path = require("path")
+const HDWalletProvider = require("@truffle/hdwallet-provider")
+const mnemonic = process.env.CRYPTO_EARTH_MNEMONIC
+const infuraProjectID = process.env.INFURA_PROJECT_ID
 
 module.exports = {
   contracts_build_directory: path.join(__dirname, "client/src/contracts"),
@@ -6,6 +9,15 @@ module.exports = {
     develop: {
       port: 7545,
       network_id: 5777,
+    },
+    ropsten: {
+      provider: function () {
+        return new HDWalletProvider(
+          mnemonic,
+          `https://ropsten.infura.io/v3/${infuraProjectID}`
+        )
+      },
+      network_id: "3",
     },
   },
 }
